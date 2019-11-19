ce3bc44842a40d0b8061c7a60e952d9140119005
Aliia <alkhasbeeva@gmail.com>
2019-11-19 19:06:06 +0300

    Find and click radiobutton and checkbox

---
+++ selenium2_1_5.py
@@ -0,0 +1,40 @@
+#Открыть страницу http://suninjuly.github.io/math.html.
+#Считать значение для переменной x.
+#Посчитать математическую функцию от x (код для этого приведён ниже).
+#Ввести ответ в текстовое поле.
+#Отметить checkbox "I'm the robot".
+#Выбрать radiobutton "Robots rule!".
+#Нажать на кнопку Submit.
+
+from selenium import webdriver
+import math
+import time
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    option1 = browser.find_element_by_id("robotCheckbox")
+    option1.click()
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(30)
+    browser.quit()
