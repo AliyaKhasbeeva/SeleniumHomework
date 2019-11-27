5ccd5afce3fafbf29427a8592839020f27c44a68
Aliia <alkhasbeeva@gmail.com>
2019-11-27 18:15:32 +0300

    Use pytest.mark.parametrize

---
+++ selenium3_6_2.py
@@ -0,0 +1,19 @@
+# PyTest позволяет запустить один и тот же тест с разными входными параметрами.
+# Для этого используется декоратор @pytest.mark.parametrize()
+
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+def test_guest_should_see_login_link(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
